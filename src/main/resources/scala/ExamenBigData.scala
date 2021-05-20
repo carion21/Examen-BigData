4045c346f32cc15d2e34736ecfee62bb6f6bb006
yann <hyperformix3@gmail.com>
2021-05-20 12:58:15 +0000

    code terminé

--- src/main/resources/scala/ExamenBigData.scala
+++ src/main/resources/scala/ExamenBigData.scala
@@ -1,10 +1,19 @@
+import java.util.*
+import scala.collection.immutable._
 
 object ExamenBigData {
 
   def main(args: Array[String]): Unit = {
-    println("bonjour")
-    val maliste=List("Java","XYZ","amen","lausann")
-    println(liste_Function(maliste))
+
+    val lt : List[String] = List("Julien", "Paul", "Jean", "rac", "trec", "joel", "ed", "chris", "maurice")
+    val ltt : List[String] = traitementListe(lt)
+
+    val ld = list_double()
+    
+    ld.foreach(elt => {
+      println(elt.head)
+      println(elt.last)
+    })
   }
 
   def traitementListe(liste : List[String]): List[String] = {
@@ -15,6 +24,40 @@ object ExamenBigData {
       }
     })
     return nliste
+  }
+
+  def list_double(): Unit = {
+
+    val listurl : List[List[String]]
+    var listesortie : List[List[String]]
+
+    listurl = List(
+      ("ecommercemag.fr", ""),
+      ("https://www.journalducm.com/contact", "payant"),
+      ("https://www.zatsaz.com/", ""),
+      ("https://www.lerevenu.com/", ""),
+      ("https://www.cadre-dirigfeant-magazine.com", "payant"),
+      ("https://www.silicon.fr/services/contact#annoncer", "payant"),
+      ("https://www.channelbiz.fr/nous-contacter/", ""),
+      ("https://www.itespresso.fr", ""),
+      ("https://www.industrie-mag.com/article4.html", "invite"),
+      ("https://www.jesuisundev.com/article-invite/", "invite"),
+      ("https://www.numerama.com/", "")
+    )
+
+    val pattern = Pattern.compile(".*'([^']*)'.*")
+
+    listurl.foreach(element => {
+      val tab : List[String]
+      val matcher = pattern.matcher(element.head)
+      if (matcher.matches) {
+        tab = Array(matcher.group(1), element.last)
+        tab :: listesortie
+      }
+    })
+    
+    return listesortie
+
   }
 
 }
