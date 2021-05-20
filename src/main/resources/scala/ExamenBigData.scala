4c1a8c80507c4897bf44cff0f540e96c2c8fb6f1
yann <hyperformix3@gmail.com>
2021-05-20 11:51:47 +0000

    fonction de manipulation de liste ok

---
+++ src/main/resources/scala/ExamenBigData.scala
@@ -0,0 +1,20 @@
+
+object ExamenBigData {
+
+  def main(args: Array[String]): Unit = {
+    println("bonjour")
+    val maliste=List("JVC","JJJ","ecolen","paysn")
+    println(liste_Function(maliste))
+  }
+
+  def traitementListe(liste : List[String]): List[String] = {
+    val nliste : List[String]
+    liste.foreach(valeur => {
+      if (valeur.matches("n$")){
+        nliste.addString(valeur)
+      }
+    })
+    return nliste
+  }
+
+}
